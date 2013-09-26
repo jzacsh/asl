98a85fcc6c86f0060c79799e95fe9f5e0aa52788
Jonathan Zacsh <jzacsh@gmail.com>
2013-09-25 23:37:56 -0400

    `yo angular` and some personal/headless Grunt/port/spec config changes.

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('spellApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
