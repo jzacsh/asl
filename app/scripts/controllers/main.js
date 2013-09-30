66e4ce0b79a891a2e4dbb51e4a0406f17c0636b2
Jonathan Zacsh <jzacsh@gmail.com>
2013-09-29 23:03:00 -0400

    Functioning hotkey listeners for "re"-spell(r) and "new" random word(n).

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -36,8 +36,52 @@ var loadWords = function(http, config) {
 };
 
 
+/**
+ * @return {boolean}
+ */
+var isActionableNode = function(nodeName) {
+  return nodeName == 'INPUT' ||
+         nodeName == 'TEXTAREA' ||
+         nodeName == 'SELECT' ||
+         nodeName == 'OPTIONS';
+};
+
+
+/**
+ * Event handler for keyup events.
+ *
+ * @param {!angular.Scope} ctrlScope
+ * @param {!Event} event
+ */
+var keyupHandler = function(ctrlScope, event) {
+  if (isActionableNode(event.target.nodeName)) {
+    return;
+  }
+
+  switch(event.keyCode) {
+    case 78:  // "n"
+      ctrlScope.spellPseudoRandomWord();
+      break;
+
+    case 82:  // "r"
+      ctrlScope.fingerSpellWord(ctrlScope.getLastWord());
+      break;
+
+    default:
+      return;
+  }
+};
+
+
 var spellAppModule = angular.module('spellApp');
-spellAppModule.controller('MainCtrl', function($http, $scope, $timeout) {
+
+spellAppModule.controller('MainCtrl', function($http, $log, $scope, $timeout, $window) {
+  $window.addEventListener(
+      'keyup', function(event) { keyupHandler($scope, event); });
+
+  /**
+   * @type {!Object.<string,string>} UI Configuration
+   */
   $scope.config = {
     letter_delay: DEFAULT_LETTER_DELAY,
     to_spell: '',
@@ -58,7 +102,6 @@ spellAppModule.controller('MainCtrl', function($http, $scope, $timeout) {
      */
     words: []
   };
-
   loadWords($http, $scope.config);
 
   /**
@@ -120,7 +163,7 @@ spellAppModule.controller('MainCtrl', function($http, $scope, $timeout) {
    */
   $scope.spellPseudoRandomWord = function() {
     if (!$scope.config.words.length) {
-      console.warning('words db not loaded yet...');
+      $log.warn('words db not loaded yet...');
     }
     var randomWord = Math.floor(Math.random() * $scope.config.words.length);
     $scope.fingerSpellWord($scope.config.words[randomWord]);
@@ -134,19 +177,27 @@ spellAppModule.controller('MainCtrl', function($http, $scope, $timeout) {
    *     A word to be finger spelled visually on the page.
    */
   $scope.fingerSpellWord = function(toSpell) {
+    // Sanity check
+    if (!toSpell || !toSpell.match(/^[a-z]*$/i)) {
+      return;
+    }
+
+    // Stop other finger-spelling sequences
+    if ($scope.delay) {
+      $timeout.cancel($scope.delay);
+    }
+
     /**
      * @type {number}
      */
     var index = 0;
-    if (!toSpell.match(/^[a-z]*$/i)) {
-      return;
-    }
-    // Save for later "previous" features.
-    $scope.respell_count = recordFingerSpelling(toSpell) - 1;
 
-    if ($scope.delay) {
-      $timeout.cancel($scope.delay);
-    }
+    /**
+     * Keep count of "respell"s of the current word.
+     *
+     * @type {number}
+     */
+    $scope.respell_count = recordFingerSpelling(toSpell) - 1;
 
     /**
      * @param {string|undefined} letter
@@ -186,7 +237,7 @@ spellAppModule.controller('MainCtrl', function($http, $scope, $timeout) {
       }, getSpellDelay(index));
     };
 
-    delaySpell(); // kick off spelling
+    delaySpell();  // kick off spelling
   };
 
   // kick off example
