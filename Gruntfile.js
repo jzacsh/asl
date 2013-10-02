8f3fa9f7e25c435c56a69d5cefe51bdfe3049939
Jonathan Zacsh <jzacsh@gmail.com>
2013-10-01 21:12:13 -0400

    make sure json data is included in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -244,7 +244,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{json,ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
